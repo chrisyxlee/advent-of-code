4c11c0b5497b943dcf5a458ff0539850ec07290d
Chris Lee <chris.yx.lee@gmail.com>
2023-12-02 15:48:37 -0800

    [2015 day 1] yay

--- 2015/src/day01/main.rs
+++ 2015/src/day01/main.rs
@@ -7,6 +7,8 @@ fn main() {
     let lines = &read_lines(file_path);
     let pt1: i64 = lines.iter().map(|line| parse_line_pt1(line)).sum();
     println!("Part 1: {}", pt1);
+    let pt2: i64 = lines.iter().map(|line| parse_line_pt2(line)).sum();
+    println!("Part 2: {}", pt2);
 }
 
 fn parse_line_pt1(s: &str) -> i64 {
@@ -15,6 +17,21 @@ fn parse_line_pt1(s: &str) -> i64 {
         ')' => sum - 1,
         _ => sum,
     });
+}
+
+fn parse_line_pt2(s: &str) -> i64 {
+    let mut floor = 0;
+    for (i, x) in s.chars().enumerate() {
+        floor += match x {
+            '(' => 1,
+            ')' => -1,
+            _ => 0,
+        };
+        if floor == -1 {
+            return (i + 1) as i64;
+        }
+    }
+    return -1;
 }
 
 fn read_lines<P>(filename: P) -> Vec<String>
@@ -48,6 +65,15 @@ mod tests {
 
         for (input, want) in tests {
             assert_eq!(parse_line_pt1(input), want, "for input {}", input);
+        }
+    }
+
+    #[test]
+    fn test_parsing_pt2() {
+        let tests = [(")", 1), ("()())", 5)];
+
+        for (input, want) in tests {
+            assert_eq!(parse_line_pt2(input), want, "for input {}", input);
         }
     }
 }
