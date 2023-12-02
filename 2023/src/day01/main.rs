12fe279e9a823c613a1fecd79a8cd526c729ea4c
Chris Lee <chris.yx.lee@gmail.com>
2023-12-02 12:14:48 -0800

    [2023 day 1] yay

--- 2023/src/day01/main.rs
+++ 2023/src/day01/main.rs
@@ -4,19 +4,77 @@ use std::path::Path;
 
 fn main() {
     let file_path = "tmp/day01/input.txt";
-    let total: i64 = read_lines(file_path)
-        .iter()
-        .map(|line| parse_line(line))
-        .sum();
-    println!("Part 1: {}", total);
-    //  println!("Part 2: {}", max.iter().sum::<usize>());
+    let lines = &read_lines(file_path);
+    let pt1: i64 = lines.iter().map(|line| parse_line_pt1(line)).sum();
+    let pt2: i64 = lines.iter().map(|line| parse_line_pt2(line)).sum();
+    println!("Part 1: {}", pt1);
+    println!("Part 1: {}", pt2);
 }
 
-fn parse_line(s: &str) -> i64 {
+fn parse_line_pt1(s: &str) -> i64 {
+    // TODO: better error checking
     let first = s.find(char::is_numeric).unwrap();
     let last = s.rfind(char::is_numeric).unwrap();
     return (s.chars().nth(first).expect("index").to_digit(10).unwrap() as i64) * 10
         + (s.chars().nth(last).expect("index").to_digit(10).unwrap() as i64);
+}
+
+fn found_to_digit(s: &str, options: &[(Option<usize>, i32)], find_min: bool) -> i64 {
+    let mut mindex: Option<usize> = None;
+    let mut mval: i64 = -1;
+    for (index, value) in options {
+        if let Some(i) = index {
+            if mindex == None
+                || (find_min && *i < mindex.unwrap())
+                || (!find_min && *i > mindex.unwrap())
+            {
+                mindex = Some(*i);
+                mval = *value as i64;
+            }
+        }
+    }
+    if mval >= 0 {
+        return mval;
+    }
+
+    return s
+        .chars()
+        .nth(mindex.unwrap())
+        .expect("index")
+        .to_digit(10)
+        .unwrap() as i64;
+}
+
+fn parse_line_pt2(s: &str) -> i64 {
+    let first = [
+        (s.find(char::is_numeric), -1),
+        (s.find("zero"), 0),
+        (s.find("one"), 1),
+        (s.find("two"), 2),
+        (s.find("three"), 3),
+        (s.find("four"), 4),
+        (s.find("five"), 5),
+        (s.find("six"), 6),
+        (s.find("seven"), 7),
+        (s.find("eight"), 8),
+        (s.find("nine"), 9),
+    ];
+    let first_digit = found_to_digit(s, &first, true);
+    let last = [
+        (s.rfind(char::is_numeric), -1),
+        (s.rfind("zero"), 0),
+        (s.rfind("one"), 1),
+        (s.rfind("two"), 2),
+        (s.rfind("three"), 3),
+        (s.rfind("four"), 4),
+        (s.rfind("five"), 5),
+        (s.rfind("six"), 6),
+        (s.rfind("seven"), 7),
+        (s.rfind("eight"), 8),
+        (s.rfind("nine"), 9),
+    ];
+    let last_digit = found_to_digit(s, &last, false);
+    return first_digit * 10 + last_digit;
 }
 
 fn read_lines<P>(filename: P) -> Vec<String>
@@ -35,7 +93,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_parsing() {
+    fn test_parsing_pt1() {
         let tests = [
             ("1abc2", 12),
             ("pqr3stu8vwx", 38),
@@ -44,7 +102,24 @@ mod tests {
         ];
 
         for (input, want) in tests {
-            assert!(parse_line(input) == want);
+            assert_eq!(parse_line_pt1(input), want);
+        }
+    }
+
+    #[test]
+    fn test_parsing_pt2() {
+        let tests = [
+            ("two1nine", 29),
+            ("eightwothree", 83),
+            ("abcone2threexyz", 13),
+            ("xtwone3four", 24),
+            ("4nineeightseven2", 42),
+            ("zoneight234", 14),
+            ("7pqrstsixteen", 76),
+        ];
+
+        for (input, want) in tests {
+            assert_eq!(parse_line_pt2(input), want);
         }
     }
 }
