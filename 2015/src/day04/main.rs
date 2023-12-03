e7f8429c468f62fc61df1df823eed94ef1fc72a6
Chris Lee <chris.yx.lee@gmail.com>
2023-12-02 16:56:12 -0800

    [2015 day 4] part 1

---
+++ 2015/src/day04/main.rs
@@ -0,0 +1,29 @@
+fn main() {
+    let secret = "ckczppom";
+    let pt1: usize = handle_pt1(secret);
+    println!("Part 1: {}", pt1);
+}
+
+fn handle_pt1(s: &str) -> usize {
+    let mut i = 0;
+    loop {
+        if format!("{:x}", md5::compute(format!("{}{}", s, i))).starts_with("00000") {
+            return i;
+        }
+        i += 1;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        let tests = [("abcdef", 609043), ("pqrstuv", 1048970)];
+
+        for (input, want) in tests {
+            assert_eq!(handle_pt1(input), want, "for input {}", input);
+        }
+    }
+}
