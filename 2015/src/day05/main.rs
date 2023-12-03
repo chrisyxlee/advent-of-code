e182be81ed834fd929610805e844dd31009eb03f
Chris Lee <chris.yx.lee@gmail.com>
2023-12-02 17:45:46 -0800

    [2015 day 5] yay

--- 2015/src/day05/main.rs
+++ 2015/src/day05/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
@@ -7,12 +8,20 @@ fn main() {
     let lines = &read_lines(file_path);
     let pt1: usize = lines
         .iter()
-        .map(|x| match is_nice(x) {
+        .map(|x| match is_nice_pt1(x) {
             true => 1,
             false => 0,
         })
         .sum();
     println!("Part 1: {}", pt1);
+    let pt2: usize = lines
+        .iter()
+        .map(|x| match is_nice_pt2(x) {
+            true => 1,
+            false => 0,
+        })
+        .sum();
+    println!("Part 2: {}", pt2);
 }
 
 /*
@@ -22,7 +31,7 @@ It contains at least three vowels (aeiou only), like .
 It contains at least one letter that appears twice in a row, like xx, abcdde (dd), or aabbccdd (aa, bb, cc, or dd).
 It does not contain the strings ab, cd, pq, or xy, even if they are part of one of the other requirements.
  */
-fn is_nice(s: &str) -> bool {
+fn is_nice_pt1(s: &str) -> bool {
     let bad = ["ab", "cd", "pq", "xy"];
     for b in bad {
         if s.find(b).is_some() {
@@ -55,6 +64,45 @@ fn is_nice(s: &str) -> bool {
     return vowel_count >= 3 && doubled;
 }
 
+/*
+It contains a pair of any two letters that appears at least twice in the string without overlapping, like xyxy (xy) or aabcdefgaa (aa), but not like aaa (aa, but it overlaps).
+It contains at least one letter which repeats with exactly one letter between them, like xyx, abcdefeghi (efe), or even aaa.
+*/
+fn is_nice_pt2(s: &str) -> bool {
+    let mut positions: HashMap<String, usize> = HashMap::new();
+
+    let mut doubled = false;
+    let mut sandwiched = false;
+
+    for (i, c) in s.chars().enumerate() {
+        if i > 0 {
+            let p = s.chars().nth(i - 1).unwrap();
+            let curr = format!("{}{}", p, c);
+
+            if let Some(pos) = positions.get(&curr) {
+                if i - 1 - *pos >= 2 {
+                    doubled = true;
+                }
+            } else {
+                positions.insert(curr, i - 1);
+            }
+        }
+
+        if i > 1 && !sandwiched {
+            let p = s.chars().nth(i - 2).unwrap();
+            if p == c {
+                sandwiched = true;
+            }
+        }
+
+        if sandwiched && doubled {
+            return true;
+        }
+    }
+
+    return false;
+}
+
 fn read_lines<P>(filename: P) -> Vec<String>
 where
     P: AsRef<Path>,
@@ -81,7 +129,28 @@ mod tests {
         ];
 
         for (input, want) in tests {
-            assert_eq!(is_nice(input), want, "for input {}", input);
+            assert_eq!(is_nice_pt1(input), want, "for input {}", input);
+        }
+    }
+
+    #[test]
+    fn test_parsing_pt2() {
+        let tests = [
+            ("qjhvhtzxzqqjkmpb", true),
+            ("xxyxx", true),
+            ("uurcxstgmygtbstg", false),
+            ("ieodomkazucvgmuy", false),
+            ("xyxy", true),
+            ("aabcdefgaa", false),
+            ("baaab", false),
+            ("abcdefeghiab", true),
+            ("xyaksllpqwoefexy", true),
+            ("aaaa", true),
+            ("aaabcb", false),
+        ];
+
+        for (input, want) in tests {
+            assert_eq!(is_nice_pt2(input), want, "for input {}", input);
         }
     }
 }
