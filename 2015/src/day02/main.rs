3e761081159b5a39e3184da7a55126afb62ff771
Chris Lee <chris.yx.lee@gmail.com>
2023-12-02 16:11:48 -0800

    [2015 day 2] yay

--- 2015/src/day02/main.rs
+++ 2015/src/day02/main.rs
@@ -6,13 +6,21 @@ use std::path::Path;
 fn main() {
     let file_path = "tmp/day02/input.txt";
     let lines = &read_lines(file_path);
-    let pt1: i64 = lines.iter().map(|line| parse_line_pt1(line)).sum();
+    let pt1: i64 = lines
+        .iter()
+        .map(|s| parse_line(s))
+        .map(|x| compute_pt1(x))
+        .sum();
     println!("Part 1: {}", pt1);
-    //  let pt2: i64 = lines.iter().map(|line| parse_line_pt2(line)).sum();
-    //  println!("Part 2: {}", pt2);
+    let pt2: i64 = lines
+        .iter()
+        .map(|s| parse_line(s))
+        .map(|x| compute_pt2(x))
+        .sum();
+    println!("Part 2: {}", pt2);
 }
 
-fn parse_line_pt1(s: &str) -> i64 {
+fn parse_line(s: &str) -> (i64, i64, i64) {
     let mut x = 0;
     let mut y = 0;
     let mut z = 0;
@@ -41,8 +49,19 @@ fn parse_line_pt1(s: &str) -> i64 {
         }
     }
 
+    return (x, y, z);
+}
+
+fn compute_pt1((x, y, z): (i64, i64, i64)) -> i64 {
     let sides = [x * y, x * z, y * z];
     return 2 * sides.iter().sum::<i64>() + sides.iter().min().unwrap();
+}
+
+fn compute_pt2((x, y, z): (i64, i64, i64)) -> i64 {
+    let mut ordered = [x, y, z];
+    ordered.sort();
+
+    return 2 * (ordered[0] + ordered[1]) + x * y * z;
 }
 
 fn read_lines<P>(filename: P) -> Vec<String>
@@ -65,7 +84,16 @@ mod tests {
         let tests = [("2x3x4", 58), ("1x1x10", 43)];
 
         for (input, want) in tests {
-            assert_eq!(parse_line_pt1(input), want, "for input {}", input);
+            assert_eq!(compute_pt1(parse_line(input)), want, "for input {}", input);
+        }
+    }
+
+    #[test]
+    fn test_parsing_pt2() {
+        let tests = [("2x3x4", 34), ("1x1x10", 14)];
+
+        for (input, want) in tests {
+            assert_eq!(compute_pt2(parse_line(input)), want, "for input {}", input);
         }
     }
 }
