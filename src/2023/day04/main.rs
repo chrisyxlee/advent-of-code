d4ee47f452601b906a7f6b26d1c4e8ccbb17a641
Chris Lee <chris.yx.lee@gmail.com>
2023-12-03 23:47:08 -0800

    [2023 day 4] part 1

---
+++ src/2023/day04/main.rs
@@ -0,0 +1,93 @@
+use advent_of_code::utils::input::read_lines;
+use clap::Parser;
+use regex::Regex;
+use std::collections::HashSet;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Input file.
+    #[arg(short, long)]
+    input: String,
+}
+
+fn main() {
+    let args = Args::parse();
+    let lines = read_lines(args.input);
+
+    let pt1: i32 = handle_pt1(&lines);
+    println!("Part 1: {}", pt1);
+}
+
+fn parse_line_pt1(line: &str) -> i32 {
+    let line_re = Regex::new(r"Card\s+(\d+):\s+(.*)\s+\|\s+(.*)").unwrap();
+    let mut winning_set: HashSet<i32> = HashSet::new();
+
+    let mut match_count = 0;
+    for m in line_re.captures_iter(line) {
+        for (i, capt) in m.iter().enumerate() {
+            if let Some(sub) = capt {
+                match i {
+                    2 => {
+                        for n in sub
+                            .as_str()
+                            .split(" ")
+                            .filter(|x| x.len() > 0)
+                            .map(|x| x.parse::<i32>().unwrap())
+                        {
+                            winning_set.insert(n);
+                        }
+                    }
+                    3 => {
+                        for n in sub
+                            .as_str()
+                            .split(" ")
+                            .filter(|x| x.len() > 0)
+                            .map(|x| x.parse::<i32>().unwrap())
+                        {
+                            if winning_set.contains(&n) {
+                                match_count += 1;
+                            }
+                        }
+                    }
+                    _ => {}
+                }
+            }
+        }
+    }
+
+    if match_count == 0 {
+        return 0;
+    }
+
+    let base: i32 = 2;
+    base.pow(match_count - 1)
+}
+
+fn handle_pt1(lines: &Vec<String>) -> i32 {
+    lines.iter().map(|l| parse_line_pt1(l)).sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parsing_pt1() {
+        let tests = [(
+            vec![
+                String::from("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"),
+                String::from("Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19"),
+                String::from("Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1"),
+                String::from("Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83"),
+                String::from("Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36"),
+                String::from("Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"),
+            ],
+            13,
+        )];
+
+        for (input, want) in tests {
+            assert_eq!(handle_pt1(&input), want, "for input\n{}", input.join("\n"));
+        }
+    }
+}
