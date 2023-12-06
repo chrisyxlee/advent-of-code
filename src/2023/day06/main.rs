a03655f5ac3c19f0b0a4a34184c313dcd714168c
Chris Lee <chris.yx.lee@gmail.com>
2023-12-05 21:15:25 -0800

    [2023 day] yay

--- src/2023/day06/main.rs
+++ src/2023/day06/main.rs
@@ -16,22 +16,21 @@ fn main() {
     Time:        41     96     88     94
     Distance:   214   1789   1127   1055
     */
-    let input = vec![(41, 214), (96, 1789), (88, 1127), (94, 1055)];
-
-    let pt1: usize = handle_pt1(&input);
+    let pt1: usize = handle_pt1(vec![(41, 214), (96, 1789), (88, 1127), (94, 1055)]);
     println!("Part 1: {}", pt1);
+    let pt2: usize = count_hold_start(41968894, 214178911271055);
     //  let pt2: i32 = handle_pt2(&lines);
-    //  println!("Part 2: {}", pt2);
+     println!("Part 2: {}", pt2);
 }
 
-fn count_hold_start(time: i32, distance: i32) -> usize {
+fn count_hold_start(time: i64, distance: i64) -> usize {
     (0..=time)
         .into_iter()
         .filter(|start_speed| start_speed * (time - start_speed) > distance)
         .count()
 }
 
-fn handle_pt1(times: &Vec<(i32, i32)>) -> usize {
+fn handle_pt1(times: Vec<(i64, i64)>) -> usize {
     let mut res = 1;
     for x in times
         .iter()
