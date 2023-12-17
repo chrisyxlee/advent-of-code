692e2f428fbb43e11f25132c8d8aec4714f92626
Chris Lee <chris.yx.lee@gmail.com>
2023-12-17 13:25:17 -0800

    [2023 day 17] part 1 checkpoint - BTreeMap is a map, not a queue lol

--- src/2023/day17/main.rs
+++ src/2023/day17/main.rs
@@ -1,7 +1,8 @@
 use advent_of_code::utils::input::read_lines;
 use advent_of_code::utils::point::Point;
 use clap::Parser;
-use std::collections::{HashMap, HashSet};
+use std::collections::{BTreeMap, HashMap, HashSet};
+use std::fmt;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -76,154 +77,175 @@ fn go(p: Point<i32>, dir: char, corner: Point<i32>) -> Option<Point<i32>> {
     Some(new_point)
 }
 
-fn backwards(dir: Option<char>, new_dir: char) -> bool {
-    new_dir
-        == match dir {
-            Some(WEST) => EAST,
-            Some(EAST) => WEST,
-            Some(NORTH) => SOUTH,
-            Some(SOUTH) => NORTH,
-            None => '?',
-            _ => unreachable!(),
-        }
+#[derive(Eq, PartialEq, Debug, Copy, Clone, Hash)]
+pub struct Visit {
+    loc: Point<i32>,
+    dir: char,
+    streak: i32,
 }
 
-fn straight(dir: Option<char>, new_dir: char) -> bool {
-    dir.is_some() && dir.unwrap() == new_dir
+impl Visit {
+    pub fn new(loc: Point<i32>, dir: char, streak: i32) -> Self {
+        Self { loc, dir, streak }
+    }
+
+    pub fn neighbors(&self, corner: Point<i32>) -> Vec<Visit> {
+        let mut res = Vec::new();
+        if self.streak < 3 {
+            if let Some(forward_loc) = go(self.loc, self.dir, corner) {
+                res.push(Visit {
+                    loc: forward_loc,
+                    dir: self.dir,
+                    streak: self.streak + 1,
+                });
+            }
+        }
+
+        let right_dir = self.right();
+        if let Some(right_loc) = go(self.loc, right_dir, corner) {
+            res.append(&mut vec![Visit {
+                loc: right_loc,
+                dir: right_dir,
+                streak: 1,
+            }]);
+        }
+
+        let left_dir = self.left();
+        if let Some(left_loc) = go(self.loc, left_dir, corner) {
+            res.append(&mut vec![Visit {
+                loc: left_loc,
+                dir: left_dir,
+                streak: 1,
+            }]);
+        }
+
+        res
+    }
+
+    fn left(&self) -> char {
+        match self.dir {
+            WEST => SOUTH,
+            SOUTH => EAST,
+            EAST => NORTH,
+            NORTH => WEST,
+            _ => unreachable!(),
+        }
+    }
+
+    fn right(&self) -> char {
+        match self.dir {
+            WEST => NORTH,
+            NORTH => EAST,
+            EAST => SOUTH,
+            SOUTH => WEST,
+            _ => unreachable!(),
+        }
+    }
+}
+
+impl fmt::Display for Visit {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "@ {} -> {} x{}", self.loc, self.dir, self.streak)
+    }
 }
 
 pub struct State {
-    min_heat: Option<i32>,
+    heat_map: HashMap<Visit, i32>,
 }
 
 impl State {
-    pub fn new() -> Self {
-        Self { min_heat: None }
-    }
-
-    pub fn register(&mut self, heat: i32) {
-        if let Some(mh) = self.min_heat {
-            if heat < mh {
-                self.min_heat = Some(heat);
-                println!("NEW MIN = {}", heat);
-            }
-        } else {
-            self.min_heat = Some(heat);
-            println!("NEW MIN = {}", heat);
-        }
-    }
-
-    pub fn futile(&self, heat: i32) -> bool {
-        self.min_heat.is_some() && heat >= self.min_heat.unwrap()
-    }
-}
-
-fn lose_heat(
-    grid: &HashMap<Point<i32>, i32>,
-    corner: Point<i32>,
-    p: Point<i32>,
-    dir: Option<char>,
-    streak: i32,
-    heat: i32,
-    state: &mut State,
-    visited: &mut HashSet<Point<i32>>,
-) {
-    visited.insert(p);
-
-    /*
-    if let Some(d) = dir {
-        println!(
-            "p = {}, dir = {}, streak = {}, heat = {}, visited = {}, {}",
-            p,
-            d,
-            streak,
-            heat,
-            visited.len(),
-            visited
-                .iter()
-                .map(|x| x.to_string())
-                .collect::<Vec<String>>()
-                .join(",")
-        );
-    } else {
-        println!(
-            "start | p = {}, streak = {}, heat = {}, visited = {}, {}",
-            p,
-            streak,
-            heat,
-            visited.len(),
-            visited
-                .iter()
-                .map(|x| x.to_string())
-                .collect::<Vec<String>>()
-                .join(",")
-        );
-    }
-    */
-
-    if p == corner {
-        //   println!("YAY {}", heat);
-        state.register(heat);
-        return;
-    }
-
-    if state.futile(heat) {
-        //   println!(
-        //       "futile {} (currently smallest = {})",
-        //       heat,
-        //       state.min_heat.unwrap()
-        //   );
-        return;
-    }
-
-    for new_dir in vec![NORTH, EAST, SOUTH, WEST] {
-        if backwards(dir, new_dir) {
-            continue;
-        }
-
-        if let Some(new_p) = go(p, new_dir, corner) {
-            let mut new_streak = 1;
-            if straight(dir, new_dir) {
-                new_streak = streak + 1;
-                if new_streak > 3 {
-                    //   println!("exceeded 3 streak {}", p);
-                    continue;
+    pub fn new(corner: Point<i32>) -> Self {
+        let mut map = HashMap::new();
+        for x in 0..=corner.x {
+            for y in 0..=corner.y {
+                for dir in vec![NORTH, EAST, WEST, SOUTH] {
+                    for streak in 1..3 {
+                        map.insert(Visit::new(Point { x: x, y: y }, dir, streak), i32::MAX);
+                    }
                 }
             }
-
-            if visited.contains(&new_p) {
-                //  println!("visited {}", new_p);
-                continue;
-            }
-
-            lose_heat(
-                grid,
-                corner,
-                new_p,
-                Some(new_dir),
-                new_streak,
-                heat + grid.get(&new_p).unwrap(),
-                state,
-                &mut visited.clone(),
-            );
         }
+
+        Self { heat_map: map }
+    }
+
+    pub fn register(&mut self, visit: Visit, heat: i32) {
+        self.heat_map
+            .entry(visit)
+            .and_modify(|e| {
+                if *e > heat {
+                    *e = heat;
+                }
+            })
+            .or_insert(heat);
+    }
+
+    pub fn futile(&self, visit: Visit, heat: i32) -> bool {
+        let maybe_min = self.heat_map.get(&visit);
+        maybe_min.is_some() && heat >= *maybe_min.unwrap()
+    }
+
+    pub fn get_min_heat(&self, loc: Point<i32>) -> i32 {
+        *self
+            .heat_map
+            .iter()
+            .filter(|(k, _)| k.loc == loc)
+            .map(|(_, v)| v)
+            .min()
+            .unwrap()
     }
 }
 
 fn handle_pt1(grid: &HashMap<Point<i32>, i32>, corner: Point<i32>) -> i32 {
-    let mut state = State::new();
-    lose_heat(
-        grid,
-        corner,
-        Point { x: 0, y: 0 },
-        None,
-        0,
-        0,
-        &mut state,
-        &mut HashSet::new(),
-    );
+    let mut state = State::new(corner);
 
-    state.min_heat.unwrap()
+    let start = Point { x: 0, y: 0 };
+
+    // Use a sorted map (sorted on key, so key needs to be heat).
+    let mut priority_queue = BTreeMap::new();
+    let right = Visit::new(go(start, EAST, corner).unwrap(), EAST, 1);
+    let right_heat = *grid.get(&right.loc).unwrap();
+    priority_queue.insert(right_heat, right);
+    state.register(right, right_heat);
+
+    let down = Visit::new(go(start, SOUTH, corner).unwrap(), SOUTH, 1);
+    let down_heat = *grid.get(&down.loc).unwrap();
+    priority_queue.insert(down_heat, down);
+    state.register(down, down_heat);
+
+    let mut visited: HashSet<Visit> = HashSet::new();
+
+    while !priority_queue.is_empty() {
+        let (heat, visit) = priority_queue.pop_first().unwrap();
+        print!("{} {} -- ", heat, visit);
+        if !visited.insert(visit) {
+            println!("BYE");
+            continue;
+        }
+
+        println!("add neighbors");
+        for neighbor in visit.neighbors(corner) {
+            let neighbor_heat = heat + grid.get(&neighbor.loc).unwrap();
+            priority_queue.insert(neighbor_heat, neighbor);
+            println!(" - neighbor has {} {}", neighbor_heat, neighbor);
+
+            state.register(neighbor, neighbor_heat);
+        }
+    }
+
+    for y in 0..=corner.y {
+        println!(
+            "{}",
+            (0..=corner.x)
+                .into_iter()
+                .map(|x| Point { x: x, y: y })
+                .map(|p| format!("{:10}({}) ", state.get_min_heat(p), grid.get(&p).unwrap()))
+                .collect::<Vec<String>>()
+                .join(" ")
+        );
+    }
+
+    state.get_min_heat(corner)
 }
 
 #[cfg(test)]
