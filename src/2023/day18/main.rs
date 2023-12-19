fb1633c8696b7b5f81ed3b89335e238e85b91f64
Chris Lee <chris.yx.lee@gmail.com>
2023-12-18 20:51:13 -0800

    [2023 day 18] part 1 checkpoint - wtf is going on

--- src/2023/day18/main.rs
+++ src/2023/day18/main.rs
@@ -2,7 +2,8 @@ use advent_of_code::utils::input::read_lines;
 use advent_of_code::utils::point::Point;
 use clap::Parser;
 use std::cmp::Ordering;
-use std::collections::{BTreeMap, BinaryHeap, HashSet};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet};
+use std::fmt;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -29,6 +30,12 @@ const RIGHT: char = 'R';
 pub struct Dig {
     steps: i64,
     dir: char,
+}
+
+impl fmt::Display for Dig {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} {}", self.steps, self.dir)
+    }
 }
 
 fn parse_line_pt1(s: &str) -> Dig {
@@ -188,34 +195,231 @@ fn handle_pt1(lines: &Vec<String>) -> i64 {
     area(&grid, bounds)
 }
 
+#[derive(Eq, PartialEq, Debug, Copy, Clone, Hash)]
+pub struct VerticalLine {
+    x: i64,
+    y_start: i64,
+    y_end: i64,
+}
+
+impl VerticalLine {
+    fn contains_y(&self, y: i64) -> bool {
+        self.y_start <= y && y <= self.y_end
+    }
+
+    fn equal_y(&self, other: &Self) -> bool {
+        self.y_start == other.y_start && self.y_end == other.y_end
+    }
+
+    fn overlaps_y(&self, other: &Self) -> bool {
+        self.contains_y(other.y_start)
+            || self.contains_y(other.y_end)
+            || other.contains_y(self.y_start)
+            || other.contains_y(self.y_end)
+    }
+}
+
+impl fmt::Display for VerticalLine {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} <-> {} @ {}", self.y_start, self.y_end, self.x)
+    }
+}
+
+impl Ord for VerticalLine {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.y_start
+            .cmp(&other.y_start)
+            .then(self.y_end.cmp(&other.y_end))
+            .then(self.x.cmp(&other.x))
+    }
+}
+
+impl PartialOrd for VerticalLine {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+fn split_vertical_ranges(lines: &Vec<VerticalLine>) -> Vec<VerticalLine> {
+    let mut curr_lines = lines.clone();
+    let mut count = 0;
+    loop {
+        println!("curr lines has length {}", curr_lines.len());
+        let mut split_lines = Vec::new();
+        let ys = curr_lines
+            .iter()
+            .map(|l| vec![l.y_start, l.y_end])
+            .flatten()
+            .collect::<Vec<i64>>();
+        while i < curr_lines.len() {
+            if group.is_empty() {
+                group.push(curr_lines[i]);
+            } else if group.last().unwrap().overlaps_y(&curr_lines[i]) {
+                group.push(curr_lines[i]);
+            }
+            if self.y_end < other.y_start {
+                return vec![*self, *other];
+            }
+
+            let mut ys = vec![self.y_start, self.y_end, other.y_start, other.y_end];
+
+            ys.sort();
+            ys.dedup();
+
+            for i in 0..ys.len() - 1 {
+                let mut y_start = ys[i];
+                let mut y_end = ys[i + 1];
+
+                if i > 0 && self.contains_y(y_start) && other.contains_y(y_start) {
+                    y_start += 1;
+                }
+
+                if i + 1 < ys.len() - 1 && !(self.contains_y(y_end) && other.contains_y(y_end)) {
+                    y_end -= 1;
+                }
+
+                if self.contains_y(y_start) && self.contains_y(y_end) {
+                    res.push(VerticalLine {
+                        x: self.x,
+                        y_start: y_start,
+                        y_end: y_end,
+                    });
+                }
+
+                if other.contains_y(y_start) && other.contains_y(y_end) {
+                    res.push(VerticalLine {
+                        x: other.x,
+                        y_start: y_start,
+                        y_end: y_end,
+                    });
+                }
+            }
+        }
+        split_lines.sort();
+        split_lines.dedup();
+
+        if split_lines.len() == curr_lines.len() {
+            println!("got it");
+            break;
+        }
+
+        curr_lines = split_lines;
+        println!(
+            "now is
+{}",
+            curr_lines
+                .iter()
+                .map(|x| x.to_string())
+                .collect::<Vec<String>>()
+                .join("\n")
+        );
+
+        count += 1;
+        if count == 2 {
+            return Vec::new();
+        }
+    }
+
+    curr_lines
+}
+
 fn handle_pt2(lines: &Vec<String>) -> i64 {
     let digs = lines
         .iter()
         .map(|l| parse_line_pt2(&l))
         .collect::<Vec<Dig>>();
 
-    let mut curr: Point<i64> = Point { x: 0, y: 0 };
-    let mut vertical_lines: BTreeMap<i64, BinaryHeap<(i64, i64)>> = BTreeMap::new();
+    println!("part 2: got digs");
+    println!(
+        "{}",
+        digs.iter()
+            .map(|x| x.to_string())
+            .collect::<Vec<String>>()
+            .join("\n")
+    );
 
+    //  let mut vertical_lines: BTreeMap<i64, BinaryHeap<(i64, i64)>> = BTreeMap::new();
     let mut space = 0;
-    for dig in digs {
-        space += dig.steps;
-        let next = jump(curr, dig.dir, dig.steps);
-        if !vec![UP, DOWN].contains(&dig.dir) {
+    let mut vertical_lines: Vec<VerticalLine> = Vec::new();
+    {
+        let mut raw_lines: Vec<VerticalLine> = Vec::new();
+        let mut curr: Point<i64> = Point { x: 0, y: 0 };
+        for dig in digs {
+            space += dig.steps;
+            let next = jump(curr, dig.dir, dig.steps);
+            println!("next = {} after {} ", next, dig);
+            if vec![UP, DOWN].contains(&dig.dir) {
+                let ys = vec![curr.y, next.y];
+                raw_lines.push(VerticalLine {
+                    x: curr.x,
+                    y_start: *ys.iter().min().unwrap(),
+                    y_end: *ys.iter().max().unwrap(),
+                });
+                // vertical_lines
+                //     .entry(min_y)
+                //     .and_modify(|e| e.push((curr.x, max_y)))
+                //     .or_insert(BinaryHeap::from(vec![line]));
+            }
+
+            curr = next;
+        }
+        raw_lines.sort();
+
+        println!("part 2: got vertical lines, spaces = {}", space);
+        println!(
+            "{}",
+            raw_lines
+                .iter()
+                .map(|x| x.to_string())
+                .collect::<Vec<String>>()
+                .join("\n")
+        );
+
+        // Split
+        vertical_lines = split_vertical_ranges(&raw_lines);
+
+        println!("part 2: split lines");
+    }
+
+    let mut i = 0;
+    let mut xs: Vec<VerticalLine> = Vec::new();
+    let mut y_now: VerticalLine = VerticalLine {
+        y_start: 0,
+        y_end: 0,
+        x: 0,
+    };
+    while i < vertical_lines.len() {
+        let curr = vertical_lines[i];
+
+        if xs.is_empty() {
+            xs.push(curr);
+            y_now = curr;
             continue;
         }
 
-        let ys = vec![curr.y, next.y];
-        let line = (*ys.iter().min().unwrap(), *ys.iter().max().unwrap());
-        vertical_lines
-            .entry(curr.x)
-            .and_modify(|e| e.push(line))
-            .or_insert(BinaryHeap::from(vec![line]));
+        if y_now.equal_y(&curr) {
+            xs.push(curr);
+            continue;
+        } else {
+            let mut l = 0;
+            while l < xs.len() {
+                if l + 1 >= xs.len() {
+                    break;
+                }
 
-        curr = next;
+                let height = xs[l].y_end - xs[l].y_start;
+                let width = xs[l + 1].x - xs[l].x;
+
+                space += height * width;
+
+                l += 2;
+            }
+
+            xs.clear();
+        }
+
+        i += 1;
     }
-    // TODO
-    // find overlap ranges -- split them up into pairs -- get the area between the pairs
 
     space
 }
@@ -259,6 +463,149 @@ mod tests {
                 "with input\n{}",
                 input.join("\n")
             );
+        }
+    }
+
+    #[test]
+    fn test_split_vertical_ranges() {
+        let tests = [(
+            vec![
+                VerticalLine {
+                    y_start: -919647,
+                    y_end: -56407,
+                    x: 818608,
+                },
+                VerticalLine {
+                    y_start: -500254,
+                    y_end: 0,
+                    x: 0,
+                },
+                VerticalLine {
+                    y_start: -56407,
+                    y_end: 0,
+                    x: 461937,
+                },
+            ],
+            vec![
+                VerticalLine {
+                    x: 0,
+                    y_start: 0,
+                    y_end: 5,
+                },
+                VerticalLine {
+                    x: 5,
+                    y_start: 10,
+                    y_end: 15,
+                },
+            ],
+        )];
+
+        for (that, want) in tests {
+            assert_eq!(
+                split_vertical_ranges(&that),
+                want,
+                "{}",
+                that.iter()
+                    .map(|x| x.to_string())
+                    .collect::<Vec<String>>()
+                    .join("\n")
+            );
+        }
+    }
+
+    #[test]
+    fn test_split() {
+        let tests = [
+            (
+                "different x, same y",
+                (
+                    VerticalLine {
+                        x: 0,
+                        y_start: 10,
+                        y_end: 15,
+                    },
+                    VerticalLine {
+                        x: 5,
+                        y_start: 10,
+                        y_end: 15,
+                    },
+                ),
+                vec![
+                    VerticalLine {
+                        x: 0,
+                        y_start: 10,
+                        y_end: 15,
+                    },
+                    VerticalLine {
+                        x: 5,
+                        y_start: 10,
+                        y_end: 15,
+                    },
+                ],
+            ),
+            (
+                "same y start, one is longer",
+                (
+                    VerticalLine {
+                        x: 0,
+                        y_start: 0,
+                        y_end: 5,
+                    },
+                    VerticalLine {
+                        x: 12,
+                        y_start: 0,
+                        y_end: 15,
+                    },
+                ),
+                vec![
+                    VerticalLine {
+                        x: 0,
+                        y_start: 0,
+                        y_end: 5,
+                    },
+                    VerticalLine {
+                        x: 12,
+                        y_start: 0,
+                        y_end: 5,
+                    },
+                    VerticalLine {
+                        x: 12,
+                        y_start: 6,
+                        y_end: 15,
+                    },
+                ],
+            ),
+            (
+                "not overlapping",
+                (
+                    VerticalLine {
+                        x: 0,
+                        y_start: 0,
+                        y_end: 5,
+                    },
+                    VerticalLine {
+                        x: 5,
+                        y_start: 10,
+                        y_end: 15,
+                    },
+                ),
+                vec![
+                    VerticalLine {
+                        x: 0,
+                        y_start: 0,
+                        y_end: 5,
+                    },
+                    VerticalLine {
+                        x: 5,
+                        y_start: 10,
+                        y_end: 15,
+                    },
+                ],
+            ),
+        ];
+
+        for (desc, (this, that), want) in tests {
+            assert_eq!(this.split(&that), want, "{}", desc);
         }
     }
 }
